7709cc14f1b16b6f0400630aba2097c32a7afc37
Bismarck Villca <bismarck.villca@gmail.com>
2025-08-17 00:38:42 -0400

    feat: update public API exports and increment version to 1.0.1
    Signed-off-by: Bismarck Villca <bismarck.villca@gmail.com>

--- projects/jhi-basic-component/src/public-api.ts
+++ projects/jhi-basic-component/src/public-api.ts
@@ -2,7 +2,13 @@
  * Public API Surface of jhi-basic-component
  */
 
-export * from './lib/active-toggle';
-export * from './lib/advanced-search';
-export * from './lib/breadcrumb';
-export * from './lib/confirm-dialog';
+export  * from './lib/active-toggle/active-toggle.component';
+
+export * from './lib/advanced-search/advanced-search.component';
+export * from './lib/advanced-search/button-search.component';
+
+export * from './lib/breadcrumb/breadcrumb.component';
+export * from './lib/breadcrumb/breadcrumb.model';
+
+export * from './lib/confirm-dialog/confirm-dialog.component';
+export * from './lib/confirm-dialog/confirm-dialog.service';
