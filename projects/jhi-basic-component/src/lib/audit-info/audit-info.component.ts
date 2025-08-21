fff46a584656d3ede9d56263258e5a02e953433e
Bismarck Villca <bismarck.villca@gmail.com>
2025-08-21 14:56:16 -0400

    feat: update package version, refactor audit info extraction, and enhance component functionality

--- projects/jhi-basic-component/src/lib/audit-info/audit-info.component.ts
+++ projects/jhi-basic-component/src/lib/audit-info/audit-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, inject } from '@angular/core';
+import { Component, input, inject, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -11,6 +11,21 @@ export interface AuditableEntity {
   lastModifiedDate?: string | Date | null;
 }
 
+/**
+ * Extrae información de auditoría de cualquier objeto
+ * Busca propiedades comunes de auditoría con diferentes nomenclaturas
+ */
+function extractAuditInfo(entity: any): AuditableEntity {
+  if (!entity) return {};
+
+  return {
+    createdBy: entity.createdBy || entity.createdUser || entity.createUser || entity.creator || null,
+    createdDate: entity.createdDate || entity.createdAt || entity.createDate || entity.creationDate || null,
+    lastModifiedBy: entity.lastModifiedBy || entity.modifiedBy || entity.updatedBy || entity.lastUpdatedBy || entity.modifier || null,
+    lastModifiedDate: entity.lastModifiedDate || entity.modifiedDate || entity.updatedAt || entity.lastUpdatedAt || entity.updateDate || null,
+  };
+}
+
 @Component({
   selector: 'jhi-audit-info',
   standalone: true,
@@ -18,14 +33,15 @@ export interface AuditableEntity {
   template: `
     <div class="audit-info" (click)="openAuditModal()">
       <div class="audit-line">
+        Hola
         <fa-icon icon="calendar-check" class="audit-icon created-icon"></fa-icon>
-        <span class="audit-user">{{ entity().createdBy || 'N/A' }}</span>
-        <span class="audit-date">{{ entity().createdDate | date: 'yyyy-MM-dd HH:mm' }}</span>
+        <span class="audit-user">{{ auditInfo().createdBy || 'N/A' }}</span>
+        <span class="audit-date">{{ auditInfo().createdDate | date: 'yyyy-MM-dd HH:mm' }}</span>
       </div>
       <div class="audit-line">
         <fa-icon icon="newspaper" class="audit-icon modified-icon"></fa-icon>
-        <span class="audit-user">{{ entity().lastModifiedBy || 'N/A' }}</span>
-        <span class="audit-date">{{ entity().lastModifiedDate | date: 'yyyy-MM-dd HH:mm' }}</span>
+        <span class="audit-user">{{ auditInfo().lastModifiedBy || 'N/A' }}</span>
+        <span class="audit-date">{{ auditInfo().lastModifiedDate | date: 'yyyy-MM-dd HH:mm' }}</span>
       </div>
       <div class="audit-action">
         <fa-icon icon="search-plus" class="action-icon" title="Click to view detailed audit information"></fa-icon>
@@ -35,7 +51,10 @@ export interface AuditableEntity {
   styleUrls: [ './audit-info.component.scss' ],
 })
 export class AuditInfoComponent {
-  entity = input.required<AuditableEntity>();
+  entity = input.required<any>();
+
+  // Computed signal que extrae automáticamente la información de auditoría
+  auditInfo = computed(() => extractAuditInfo(this.entity()));
 
   private modalService = inject(NgbModal);
 
@@ -47,6 +66,6 @@ export class AuditInfoComponent {
       centered: true,
     });
 
-    modalRef.componentInstance.entity = this.entity;
+    modalRef.componentInstance.entity = this.auditInfo;
   }
 }
